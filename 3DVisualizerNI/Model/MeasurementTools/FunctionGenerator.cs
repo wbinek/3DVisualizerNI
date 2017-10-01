22a82a56e67edba1689767600ee9bba1e5b6a083
wbinek <wbinek@agh.edu.pl>
2017-10-01 21:06:19 +0200

    Add silence after output

--- 3DVisualizerNI/Model/MeasurementTools/FunctionGenerator.cs
+++ 3DVisualizerNI/Model/MeasurementTools/FunctionGenerator.cs
@@ -56,12 +56,15 @@ namespace _3DVisualizerNI.Model.MeasurementTools
         {
             int lengthSequence = (signal.Length + breakLength);
             double[] silence = new double[breakLength];
-            double[] output = new double[lengthSequence * repetitions];
+            double[] output = new double[lengthSequence * repetitions + (int)(0.2*signal.Length)];
             for (int i = 0; i < repetitions; i++)
             {
                 signal.CopyTo(output,i*lengthSequence);
                 silence.CopyTo(output, (i * lengthSequence) + signal.Length);
             }
+
+            signal.Take((int)(0.2 * signal.Length)).ToArray().CopyTo(output, repetitions * lengthSequence);
+
             return output;
         }
 
